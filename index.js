e954a6fb7710a0d44cf78e6e776827f767c0a278
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2022-12-17 00:03:44 -0500

    commit tree 1429a49d77386e974921a919fce03da6f4b47a9d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+var text = document.createTextNode("Judy is the champion");
+newHeader.appendChild(text);
